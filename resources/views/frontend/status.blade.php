fd5099f1abc58a0a1cbbf85e6b1282b0dc6410dc
hazimah-uitm <hazimahpethie@uitm.edu.my>
2025-04-16 11:47:18 +0800

    split file

---
+++ resources/views/frontend/status.blade.php
@@ -0,0 +1,171 @@
+        <section id="status" class="pricing section">
+            <!-- Section Title -->
+            <div class="container section-title" data-aos="fade-up">
+                <h2>Status</h2>
+                <div><span>Status Semasa Liputan</span> <span class="description-title">WiFi</span></div>
+            </div><!-- End Section Title -->
+            <!-- WiFi Coverage Chart -->
+            <div class="container" data-aos="fade-up">
+                <div class="card">
+                    <div class="card-header text-center text-white h6" style="background-color: #03244c;">
+                        LIPUTAN WIFI MENGIKUT KAMPUS
+                    </div>
+                </div>
+                <div style="height: 220px;">
+                    <canvas id="liputanChart" style="width: 100%; height: 100%;"></canvas>
+                </div>
+            </div>
+            <div class="container" data-aos="fade-up">
+                <div class="card mt-3 mb-2">
+                    <div class="card-header text-center text-white h6" style="background-color: #03244c;">
+                        LIPUTAN WIFI MENGIKUT KAWASAN LIPUTAN
+                    </div>
+                </div>
+                <div class="row gy-4">
+                    <!-- SAMARAHAN CAMPUS -->
+                    <div class="col-lg-4" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="pricing-item shadow-sm p-4 bg-light rounded">
+                            <h6 class="text-center fw-bold mb-3 text-primary text-uppercase">Samarahan</h6>
+                            <div class="d-flex flex-column">
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Akademik</span> <span>26%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-success" role="progressbar" style="width: 26%"
+                                            aria-valuenow="26" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Kolej Kediaman Pelajar</span> <span>100%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: 100%"
+                                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Pentadbiran</span> <span>65%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 65%"
+                                            aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Tumpuan dan Gunasama</span> <span>68%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 68%"
+                                            aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- SAMARAHAN 2 CAMPUS -->
+                    <div class="col-lg-4" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="pricing-item shadow-sm p-4 bg-light rounded">
+                            <h6 class="text-center fw-bold mb-3 text-primary text-uppercase">Samarahan 2</h6>
+                            <div class="d-flex flex-column">
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Akademik</span> <span>18%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-success" role="progressbar" style="width: 18%"
+                                            aria-valuenow="18" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Kolej Kediaman Pelajar</span> <span>49%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: 49%"
+                                            aria-valuenow="49" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Pentadbiran</span> <span>59%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 59%"
+                                            aria-valuenow="59" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Tumpuan dan Gunasama</span> <span>58%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 58%"
+                                            aria-valuenow="58" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- MUKAH CAMPUS -->
+                    <div class="col-lg-4" data-aos="zoom-in" data-aos-delay="300">
+                        <div class="pricing-item shadow-sm p-4 bg-light rounded">
+                            <h6 class="text-center fw-bold mb-3 text-primary text-uppercase">Mukah</h6>
+                            <div class="d-flex flex-column">
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Akademik</span> <span>100%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%"
+                                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Kolej Kediaman Pelajar</span> <span>100%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-info" role="progressbar" style="width: 100%"
+                                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Pentadbiran</span> <span>100%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 100%"
+                                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+
+                                <div class="progress-area mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <span>Tumpuan dan Gunasama</span> <span>100%</span>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 100%"
+                                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section><!-- /status Section -->
