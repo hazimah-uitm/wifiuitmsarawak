cd583a40269676c41f99da3e6070cf677e4bfe20
hazimah-uitm <hazimahpethie@uitm.edu.my>
2025-04-16 15:30:29 +0800

    Add collapsable

--- resources/views/frontend/pelaninduk.blade.php
+++ resources/views/frontend/pelaninduk.blade.php
@@ -1,37 +1,54 @@
 <section id="pelaninduk" class="details section">
-    <!-- Section Title -->
     <div class="container section-title" data-aos="fade-up">
       <h2>Pelan Induk</h2>
       <div><span>Pelan Induk Naik Taraf Sistem Rangkaian</span> <span class="description-title">WiFi</span></div>
-    </div><!-- End Section Title -->
+    </div>
   
     <div class="container">
-      <!-- Samarahan -->
-      <div class="row gy-4 align-items-center features-item">
-        <div class="col-md-12 text-center" data-aos="zoom-out" data-aos-delay="100">
-          <h4>Kampus Samarahan</h4>
-          <img src="{{ asset('public/assets/img/pelan_induk_s1.png') }}" class="img-fluid" style="max-width: 80%; height: auto;" alt="Pelan Induk Samarahan">
+      <div class="accordion" id="pelanIndukAccordion">
+  
+        <!-- Samarahan -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingSamarahan">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSamarahan" aria-expanded="true" aria-controls="collapseSamarahan">
+              Kampus Samarahan
+            </button>
+          </h2>
+          <div id="collapseSamarahan" class="accordion-collapse collapse show" aria-labelledby="headingSamarahan" data-bs-parent="#pelanIndukAccordion">
+            <div class="accordion-body text-center">
+              <img src="{{ asset('public/assets/img/pelan_induk_s1.png') }}" class="img-fluid" style="max-width: 100%;" alt="Pelan Induk Samarahan">
+            </div>
+          </div>
         </div>
-      </div>
   
-      <hr class="my-5">
-  
-      <!-- Samarahan 2 -->
-      <div class="row gy-4 align-items-center features-item">
-        <div class="col-md-12 text-center" data-aos="zoom-out" data-aos-delay="100">
-          <h4>Kampus Samarahan 2</h4>
-          {{-- <img src="{{ asset('public/assets/img/pelan_induk_s2.png') }}" class="img-fluid" alt="Pelan Induk Samarahan 2"> --}}
+        <!-- Samarahan 2 -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingSamarahan2">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSamarahan2" aria-expanded="false" aria-controls="collapseSamarahan2">
+              Kampus Samarahan 2
+            </button>
+          </h2>
+          <div id="collapseSamarahan2" class="accordion-collapse collapse" aria-labelledby="headingSamarahan2" data-bs-parent="#pelanIndukAccordion">
+            <div class="accordion-body text-center">
+              <img src="{{ asset('public/assets/img/pelan_induk_s2.png') }}" class="img-fluid" style="max-width: 80%;" alt="Pelan Induk Samarahan 2">
+            </div>
+          </div>
         </div>
-      </div>
   
-      <hr class="my-5">
-  
-      <!-- Mukah -->
-      <div class="row gy-4 align-items-center features-item">
-        <div class="col-md-12 text-center" data-aos="zoom-out" data-aos-delay="100">
-          <h4>Kampus Mukah</h4>
-          {{-- <img src="{{ asset('public/assets/img/pelan_induk_mukah.png') }}" class="img-fluid" alt="Pelan Induk Mukah"> --}}
+        <!-- Mukah -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingMukah">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMukah" aria-expanded="false" aria-controls="collapseMukah">
+              Kampus Mukah
+            </button>
+          </h2>
+          <div id="collapseMukah" class="accordion-collapse collapse" aria-labelledby="headingMukah" data-bs-parent="#pelanIndukAccordion">
+            <div class="accordion-body text-center">
+              <img src="{{ asset('public/assets/img/pelan_induk_mukah.png') }}" class="img-fluid" style="max-width: 80%;" alt="Pelan Induk Mukah">
+            </div>
+          </div>
         </div>
+  
       </div>
     </div>
   </section>
