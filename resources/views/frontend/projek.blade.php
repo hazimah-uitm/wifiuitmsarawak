b29c407e6e6ef3c58e64d64478406fe86d2e3d33
hazimah-uitm <hazimahpethie@uitm.edu.my>
2025-04-18 12:58:33 +0800

    Updated mkh pelan induk

--- resources/views/frontend/projek.blade.php
+++ resources/views/frontend/projek.blade.php
@@ -12,17 +12,6 @@
         <div class="col-md-5 order-1 order-md-2 d-flex align-items-center" data-aos="zoom-out">
           <img src="{{ asset('public/assets/img/details-4.png') }}" class="img-fluid" alt="">
         </div>
-        <div class="col-md-7 order-2 order-md-1" data-aos="fade-up">
-          <h3>Quas et necessitatibus eaque impedit ipsum animi consequatur incidunt in</h3>
-          <p class="fst-italic">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
-          </p>
-          <p>
-            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
         </div>
       </div>
     </div>
--- resources/views/frontend/tatacara.blade.php
+++ resources/views/frontend/tatacara.blade.php
@@ -13,13 +13,6 @@
           <img src="{{ asset('public/assets/img/details-3.png') }}" class="img-fluid" alt="">
         </div>
         <div class="col-md-7" data-aos="fade-up">
-          <h3>Sunt consequatur ad ut est nulla consectetur reiciendis animi voluptas</h3>
-          <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>
-          <ul>
-            <li><i class="bi bi-check"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
-            <li><i class="bi bi-check"></i><span> Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
-            <li><i class="bi bi-check"></i> <span>Facilis ut et voluptatem aperiam. Autem soluta ad fugiat</span>.</li>
-          </ul>
         </div>
       </div>
     </div>
